6af5fdeb79d909908efbaa664da9c5f58f6971a4
vtimofeev01 <timofeev.vasili@gmail.com>
2023-02-17 07:33:37 +0300

    1

--- config.py
+++ config.py
@@ -21,6 +21,15 @@ class Config:
 
 class DevelopmentConfig(Config):
     # DEBUG = True
+    SQLALCHEMY_POOL_RECYCLE = 35  # value less than backend’s timeout
+    SQLALCHEMY_POOL_TIMEOUT = 7  # value less than backend’s timeout
+    SQLALCHEMY_PRE_PING = True
+    SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': SQLALCHEMY_POOL_RECYCLE, 'pool_pre_ping': SQLALCHEMY_PRE_PING}
+
+    # SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': SQLALCHEMY_POOL_RECYCLE, 'pool_timeout': SQLALCHEMY_POOL_TIMEOUT,
+    #                              'pool_pre_ping': SQLALCHEMY_PRE_PING}
+
+
     SQLALCHEMY_DATABASE_URI = create_sqlite_uri("todolist-dev.db")
 
 
